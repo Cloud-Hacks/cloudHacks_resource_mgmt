0844ae1d4f799dff3f7eea2078c799793e11d7c1
afzalbin64 <afzal@kloudone.com>
2021-12-25 20:22:16 +0530

    adds js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const client = require('twilio')(accountSid, authToken);
 client.messages 
       .create({ 
          body: 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered today. Details: http://www.baristobakery.com/', 
-         from: from_whatsapp_no,       
-         to: to_whatsapp_no
+         from: 'whatsapp:+14155238886',       
+         to: 'whatsapp:'+to_whatsapp_no
  
        }) 
       .then(message => console.log(message.sid)) 
