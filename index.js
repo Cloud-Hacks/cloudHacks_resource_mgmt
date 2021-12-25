4ebd4bdc688e4578b87c77797925379ad6f82153
afzalbin64 <afzal@kloudone.com>
2021-12-25 20:02:46 +0530

    updates js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const accountSid = os.environ['account_sid']; 
-const authToken = os.environ['auth_token']; 
-const to_whatsapp_no = os.environ['to_whatsapp_no'];
-const from_whatsapp_no = os.environ['from_whatsapp_no'];
+const accountSid = 'ACc0efcb44a2df7fc58b302b0507592246'; 
+const authToken = process.env['auth_token'];
+const to_whatsapp_no = process.env['to_whatsapp_no'];
+const from_whatsapp_no = process.env['from_whatsapp_no'];
 
 const client = require('twilio')(accountSid, authToken); 
  
